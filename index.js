138e7493eee552dfe7b9e16139cbb2238ec58be6
Mehmet Kesimalioğlu <mkesim96@gmail.com>
2022-05-29 17:44:48 +0300

    Commit

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.get('/',(req, res) => {
 })
 
 app.get('/haberler',(req, res) => {
-    axios.get('https://www.haberler.com/')
+    axios.get('https://guncelkesintiler.com/hatay/elektrik-kesintisi/')
         .then((response) =>{
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("")', html).each(function(){
+            $('a:contains("iskenderun")', html).each(function(){
 
                 const title = $(this).text()
                 const url = $(this).attr('href')
@@ -31,7 +31,7 @@ app.get('/haberler',(req, res) => {
 
 
             })
-            res.json(articles)
+        res.json(articles)
         }).catch((err) => console.log(err))
 
 })
